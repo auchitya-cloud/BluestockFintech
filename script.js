51bf5ff0b029d16aa1156f2d48ac5e9a710be10d
Auchitya <76764350+auchitya-cloud@users.noreply.github.com>
2024-11-27 21:44:31 +0530

    Update and rename script.js to text.txt

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".toggle").forEach(button => {
-    button.addEventListener("click", () => {
-        const answer = button.parentElement.nextElementSibling;
-        const isVisible = answer.style.display === "block";
-
-        answer.style.display = isVisible ? "none" : "block";
-        button.textContent = isVisible ? "+" : "-";
-    });
-});
